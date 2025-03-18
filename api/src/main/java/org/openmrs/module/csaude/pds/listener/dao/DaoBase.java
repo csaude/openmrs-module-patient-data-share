f76e52b016bf6bd0e12bece8dd356e25042da095
Daniel Chirinda <chirindzadaniel@gmail.com>
2025-03-18 18:06:42 +0200

    EC-FIX-SESSION  - Manually open session and close to avoid timeout

--- api/src/main/java/org/openmrs/module/csaude/pds/listener/dao/DaoBase.java
+++ api/src/main/java/org/openmrs/module/csaude/pds/listener/dao/DaoBase.java
@@ -1,17 +1,35 @@
 package org.openmrs.module.csaude.pds.listener.dao;
 
-import org.openmrs.api.db.hibernate.DbSession;
-import org.openmrs.api.db.hibernate.DbSessionFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DaoBase {
 	
-	protected DbSessionFactory sessionFactory;
+	protected SessionFactory sessionFactory;
 	
-	public void setSessionFactory(DbSessionFactory sessionFactory) {
+	private static final Logger logger = LoggerFactory.getLogger(DaoBase.class);
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
-	
-	public DbSession getSession() {
-		return sessionFactory.getCurrentSession();
+
+	public static <T> T executeWithTransaction(SessionFactory sessionFactory, HibernateTransactionCallback<T> callback) {
+		Transaction transaction = null;
+		try (Session session = sessionFactory.openSession()) {
+			transaction = session.beginTransaction();
+			T result = callback.doInTransaction(session);
+			transaction.commit();
+			return result;
+		}
+		catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			logger.error("Transaction failed, rolled back", e);
+			throw new RuntimeException(e);
+		}
 	}
 }
--- api/src/main/java/org/openmrs/module/csaude/pds/listener/dao/DemographicDataQueueDao.java
+++ api/src/main/java/org/openmrs/module/csaude/pds/listener/dao/DemographicDataQueueDao.java
@@ -1,12 +1,11 @@
 package org.openmrs.module.csaude.pds.listener.dao;
 
 import org.hibernate.Criteria;
-import org.hibernate.Transaction;
+import org.hibernate.FetchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.openmrs.Patient;
 import org.openmrs.PersonAttribute;
-import org.openmrs.api.db.hibernate.DbSession;
 import org.openmrs.module.csaude.pds.listener.config.utils.PdsUtils;
 import org.openmrs.module.csaude.pds.listener.dto.PatientSateDTO;
 import org.openmrs.module.csaude.pds.listener.entity.DemographicDataOffset;
@@ -28,108 +27,113 @@ public class DemographicDataQueueDao extends DaoBase {
 	private static final Logger logger = LoggerFactory.getLogger(DemographicDataQueueDao.class);
 	
 	public void createDemographicDataQueue(DemographicDataQueue demographicDataQueue) throws RuntimeException {
-		DbSession session = getSession();
-		
-		try {
-			logger.debug("Saving Patient demographic data:  uuid {} ", demographicDataQueue.getPatientUuid());
-			session.saveOrUpdate(demographicDataQueue);
-		}
-		catch (Exception e) {
-			throw new RuntimeException(
-			        "An error occurred saving patient demographic data : " + "uuid " + demographicDataQueue.getPatientUuid(),
-			        e);
-		}
-		
+		executeWithTransaction(sessionFactory, session -> {
+			try {
+				logger.debug("Saving Patient demographic data:  uuid {} ", demographicDataQueue.getPatientUuid());
+				session.saveOrUpdate(demographicDataQueue);
+			}
+			catch (Exception e) {
+				throw new RuntimeException("An error occurred saving patient demographic data : " + "uuid "
+				        + demographicDataQueue.getPatientUuid(), e);
+			}
+			return null;
+		});
 	}
 	
 	public List<PersonAttribute> getPersonAttributeByPersonType(String personAttributeTypeUuid, Integer personId) {
-		DbSession session = getSession();
-		Criteria criteria = session.createCriteria(PersonAttribute.class, "personAttribute");
-		criteria.add(Restrictions.eq("personAttribute.person.personId", personId));
-		criteria.createAlias("personAttribute.attributeType", "personAttributeType");
-		criteria.add(Restrictions.eq("personAttributeType.uuid", personAttributeTypeUuid));
-		
-		return (List<PersonAttribute>) criteria.list();
-		
+		return executeWithTransaction(sessionFactory, session -> {
+			Criteria criteria = session.createCriteria(PersonAttribute.class, "personAttribute");
+			criteria.add(Restrictions.eq("personAttribute.person.personId", personId));
+			criteria.createAlias("personAttribute.attributeType", "personAttributeType");
+			criteria.add(Restrictions.eq("personAttributeType.uuid", personAttributeTypeUuid));
+			
+			return (List<PersonAttribute>) criteria.list();
+			
+		});
 	}
 	
 	public List<DemographicDataQueue> getAllDemographicDataQueues(Integer count,
 	        DemographicDataOffset demographicDataOffset) {
-		DbSession session = getSession();
-		Criteria criteria = session.createCriteria(DemographicDataQueue.class);
 		
-		if (demographicDataOffset != null) {
-			criteria.add(Restrictions.gt("id", demographicDataOffset.getFirstRead()));
-		}
-		if (count != null) {
-			criteria.setMaxResults(count);
-		}
-		criteria.add(Restrictions.eq("active", true));
-		criteria.addOrder(Order.asc("id"));
-		
-		return (List<DemographicDataQueue>) criteria.list();
-		
+		return executeWithTransaction(sessionFactory, session -> {
+			
+			Criteria criteria = session.createCriteria(DemographicDataQueue.class);
+			
+			if (demographicDataOffset != null) {
+				criteria.add(Restrictions.gt("id", demographicDataOffset.getFirstRead()));
+			}
+			if (count != null) {
+				criteria.setMaxResults(count);
+			}
+			criteria.add(Restrictions.eq("active", true));
+			criteria.addOrder(Order.asc("id"));
+			
+			return (List<DemographicDataQueue>) criteria.list();
+			
+		});
 	}
 	
 	public Set<Patient> getPatientsByIds(List<Integer> patientIds) {
-		DbSession session = getSession();
-		Criteria criteria = session.createCriteria(Patient.class, "patient");
-		criteria.add(Restrictions.in("patient.patientId", patientIds));
-		
-		return new HashSet<>(criteria.list());
+		return executeWithTransaction(sessionFactory, session -> {
+			
+			Criteria criteria = session.createCriteria(Patient.class, "patient");
+			criteria.add(Restrictions.in("patient.patientId", patientIds));
+			criteria.setFetchMode("identifiers", FetchMode.JOIN);
+			
+			return new HashSet<>(criteria.list());
+		});
 		
 	}
 	
 	public DemographicDataOffset getDemographicDataOffset(String clientName) {
-		DbSession session = getSession();
-		Criteria criteria = session.createCriteria(DemographicDataOffset.class, "offset");
-		criteria.add(Restrictions.eq("offset.clientName", clientName));
-		
-		return (DemographicDataOffset) criteria.uniqueResult();
+		return executeWithTransaction(sessionFactory, session -> {
+			
+			Criteria criteria = session.createCriteria(DemographicDataOffset.class, "offset");
+			criteria.add(Restrictions.eq("offset.clientName", clientName));
+			
+			return (DemographicDataOffset) criteria.uniqueResult();
+		});
 	}
 	
 	public void updateOrSaveDemographicOffset(DemographicDataOffset demographicDataOffset) {
-		
-		Transaction transaction = null;
-		try {
-			DbSession session = getSession();
-			transaction = session.beginTransaction();
-			
-			logger.debug("Saving Patient demographic offset: {} for client: {}",
-			    " Start from " + demographicDataOffset.getFirstRead() + "to " + demographicDataOffset.getLastRead(),
-			    demographicDataOffset.getClientName());
-			
-			if (demographicDataOffset.isCreated()) {
+		executeWithTransaction(sessionFactory, session -> {
+			try {
 				
-				Query query = session.createQuery(
-				    "UPDATE DemographicDataOffset SET firstRead=:firstRead, lastRead=:lastRead WHERE id = :id");
-				query.setParameter("firstRead", demographicDataOffset.getFirstRead());
-				query.setParameter("lastRead", demographicDataOffset.getLastRead());
-				query.setParameter("id", demographicDataOffset.getId());
-				query.executeUpdate();
-			} else {
-				session.save(demographicDataOffset);
+				logger.debug("Saving Patient demographic offset: {} for client: {}",
+				    " Start from " + demographicDataOffset.getFirstRead() + "to " + demographicDataOffset.getLastRead(),
+				    demographicDataOffset.getClientName());
+				
+				if (demographicDataOffset.isCreated()) {
+					
+					Query query = session.createQuery(
+					    "UPDATE DemographicDataOffset SET firstRead=:firstRead, lastRead=:lastRead WHERE id = :id");
+					query.setParameter("firstRead", demographicDataOffset.getFirstRead());
+					query.setParameter("lastRead", demographicDataOffset.getLastRead());
+					query.setParameter("id", demographicDataOffset.getId());
+					query.executeUpdate();
+				} else {
+					session.save(demographicDataOffset);
+				}
 			}
-			transaction.commit();
-		}
-		catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
+			catch (Exception e) {
+				throw new RuntimeException("An error occurred saving patient demographic offset : " + " Start from "
+				        + demographicDataOffset.getFirstRead() + "to " + demographicDataOffset.getLastRead()
+				        + " for client: " + demographicDataOffset.getClientName(), e);
 			}
-			throw new RuntimeException("An error occurred saving patient demographic offset : " + " Start from "
-			        + demographicDataOffset.getFirstRead() + "to " + demographicDataOffset.getLastRead() + " for client: "
-			        + demographicDataOffset.getClientName(), e);
-		}
+			return null;
+		});
 	}
 	
 	public List<PatientSateDTO> fetchPatientState(Integer patientId) throws IOException {
-		DbSession session = getSession();
-		Path sqlPath = new ClassPathResource("queries/patient_state.sql").getFile().toPath();
-		String sql = Files.readString(sqlPath);
+		return executeWithTransaction(sessionFactory, session -> {
+			Path sqlPath = new ClassPathResource("queries/patient_state.sql").getFile().toPath();
+			String sql = Files.readString(sqlPath);
+			
+			var query = session.createSQLQuery(sql);
+			query.setParameter("patientId", patientId);
+			List<Object[]> rel = query.list();
+			return PdsUtils.getPatientSates(rel);
+		});
 		
-		var query = session.createSQLQuery(sql);
-		query.setParameter("patientId", patientId);
-		List<Object[]> rel = query.list();
-		return PdsUtils.getPatientSates(rel);
 	}
 }
